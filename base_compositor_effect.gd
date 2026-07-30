3c6d62ea562cdd1eec6a658c52264d33ea4af70d
Garrett Gunnell <gunnellg@oregonstate.edu>
2026-07-30 15:23:45 -0700

    Base compositor effect that samples color, depth, and motion vectors

--- base_compositor_effect.gd
+++ base_compositor_effect.gd
@@ -6,16 +6,28 @@ class_name BaseCompositorEffect
 @export_tool_button("Recompile", "Callable") var recompile_action = compile_shader;
 
 
-var rd: RenderingDevice
-var shader: RID
-var pipeline: RID
+var rd : RenderingDevice;
+var shader : RID;
+var pipeline : RID;
+
+var nearest_sampler : RID;
 
 
 func _init():
 	effect_callback_type = CompositorEffect.EFFECT_CALLBACK_TYPE_POST_TRANSPARENT;
+	needs_motion_vectors = true;
+	needs_normal_roughness = true;
+	
 	rd = RenderingServer.get_rendering_device();
+	if not rd: return;
 	
 	compile_shader();
+	
+	# Create nearest neighbor sampler state
+	var sampler_state : RDSamplerState = RDSamplerState.new();
+	sampler_state.min_filter = RenderingDevice.SAMPLER_FILTER_NEAREST;
+	sampler_state.mag_filter = RenderingDevice.SAMPLER_FILTER_NEAREST;
+	nearest_sampler = rd.sampler_create(sampler_state);
 
 
 func _render_callback(effect_callback_type: int, render_data: RenderData) -> void:
@@ -40,22 +52,37 @@ func _render_callback(effect_callback_type: int, render_data: RenderData) -> voi
 	push_constant.push_back(0.0);
 	push_constant.push_back(0.0);
 	
-	var input_image = render_scene_buffers.get_color_layer(0);
+	var color_buffer : RID = render_scene_buffers.get_color_layer(0);
+	var motion_vector_buffer : RID = render_scene_buffers.get_velocity_layer(0);
+	var depth_buffer : RID = render_scene_buffers.get_depth_layer(0);
 	
 	# Create a uniform set.
-	var uniform: RDUniform = RDUniform.new()
-	uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_IMAGE
-	uniform.binding = 0
-	uniform.add_id(input_image)
-	var uniform_set = UniformSetCacheRD.get_cache(shader, 0, [ uniform ])
+	var color_buffer_uniform : RDUniform = RDUniform.new();
+	color_buffer_uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_IMAGE;
+	color_buffer_uniform.binding = 0;
+	color_buffer_uniform.add_id(color_buffer);
+	
+	# Depth cannot be bound as image2D due to being created without the proper usage flag, it must be accessed with a sampler2D instead
+	var depth_buffer_uniform : RDUniform = RDUniform.new();
+	depth_buffer_uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_SAMPLER_WITH_TEXTURE;
+	depth_buffer_uniform.binding = 1;
+	depth_buffer_uniform.add_id(nearest_sampler);
+	depth_buffer_uniform.add_id(depth_buffer);
+	
+	var motion_buffer_uniform : RDUniform = RDUniform.new();
+	motion_buffer_uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_IMAGE;
+	motion_buffer_uniform.binding = 2;
+	motion_buffer_uniform.add_id(motion_vector_buffer);
+	
+	var uniform_set : RID = UniformSetCacheRD.get_cache(shader, 0, [ color_buffer_uniform, depth_buffer_uniform, motion_buffer_uniform ]);
 
 	# Run our compute shader.
-	var compute_list:= rd.compute_list_begin()
-	rd.compute_list_bind_compute_pipeline(compute_list, pipeline)
-	rd.compute_list_bind_uniform_set(compute_list, uniform_set, 0)
-	rd.compute_list_set_push_constant(compute_list, push_constant.to_byte_array(), push_constant.size() * 4)
-	rd.compute_list_dispatch(compute_list, x_groups, y_groups, z_groups)
-	rd.compute_list_end()
+	var compute_list := rd.compute_list_begin();
+	rd.compute_list_bind_compute_pipeline(compute_list, pipeline);
+	rd.compute_list_bind_uniform_set(compute_list, uniform_set, 0);
+	rd.compute_list_set_push_constant(compute_list, push_constant.to_byte_array(), push_constant.size() * 4);
+	rd.compute_list_dispatch(compute_list, x_groups, y_groups, z_groups);
+	rd.compute_list_end();
 
 
 func compile_shader() -> bool:
@@ -89,15 +116,13 @@ func compile_shader() -> bool:
 	return pipeline.is_valid();
 
 
-
-
 func _notification(what):
 	if what == NOTIFICATION_PREDELETE:
 		if shader.is_valid():
 			rd.free_rid(shader);
-
-
-
+		
+		if nearest_sampler.is_valid():
+			rd.free_rid(nearest_sampler);
 
 
 const template_shader: String = """
@@ -107,6 +132,8 @@ const template_shader: String = """
 layout(local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
 
 layout(rgba16f, set = 0, binding = 0) uniform image2D color_image;
+layout(set = 0, binding = 1) uniform sampler2D depth_image;
+layout(rgba16f, set = 0, binding = 2) uniform image2D motion_image;
 
 // Our push constant
 layout(push_constant, std430) uniform Params {
@@ -116,16 +143,25 @@ layout(push_constant, std430) uniform Params {
 
 // The code we want to execute in each invocation
 void main() {
-	ivec2 uv = ivec2(gl_GlobalInvocationID.xy);
+	ivec2 thread_id = ivec2(gl_GlobalInvocationID.xy);
 	ivec2 size = ivec2(params.raster_size);
 
-	if (uv.x >= size.x || uv.y >= size.y) {
+	if (thread_id.x >= size.x || thread_id.y >= size.y) {
 		return;
 	}
-
-	vec4 color = imageLoad(color_image, uv);
 	
+	vec2 uv = gl_GlobalInvocationID.xy / params.raster_size;
 
-	imageStore(color_image, uv, color);
+	vec4 color = imageLoad(color_image, thread_id);
+    vec4 motion = imageLoad(motion_image, thread_id); 
+	
+	float raw_depth = texture(depth_image, uv).r;
+
+	
+	vec4 color_output = color;
+	vec4 depth_output = vec4(raw_depth);
+	vec4 motion_output = abs(motion);
+
+	imageStore(color_image, thread_id, motion_output);
 }
 """
