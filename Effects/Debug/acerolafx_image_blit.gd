a99542bc041a95933786a6275120aa9dd52e43cd
Garrett Gunnell <gunnellg@oregonstate.edu>
2026-07-31 14:38:56 -0700

    Add base file for blitting images to the screen

--- Effects/Debug/acerolafx_image_blit.gd
+++ Effects/Debug/acerolafx_image_blit.gd
@@ -1,23 +1,22 @@
 @tool
 extends CompositorEffect
-class_name AcerolaFX_Blur
+class_name AcerolaFX_ImageBlit
 
 
-@export var kernel_size : int = 1;
+@export_tool_button("Recompile", "Callable") var recompile_action = compile_shader;
+
 
 var rd : RenderingDevice;
 var shader : RID;
 var pipeline : RID;
 
 var nearest_sampler : RID;
-var pong_texture : RID;
-var pong_size : Vector2i = Vector2i.ZERO;
 
-@export_tool_button("Recompile", "Callable") var recompile_action = compile_shader;
 
 func _init():
 	effect_callback_type = CompositorEffect.EFFECT_CALLBACK_TYPE_POST_TRANSPARENT;
 	needs_motion_vectors = true;
+	needs_normal_roughness = true;
 	
 	rd = RenderingServer.get_rendering_device();
 	if not rd: return;
@@ -41,32 +40,21 @@ func _render_callback(effect_callback_type: int, render_data: RenderData) -> voi
 	var render_size : Vector2i = render_scene_buffers.get_internal_size();
 	if render_size.x == 0 and render_size.y == 0: return;
 	
-	# Regenerate pong texture
-	if pong_size != render_size:
-		if pong_texture.is_valid(): rd.free_rid(pong_texture);
-		
-		var pong_format : RDTextureFormat = RDTextureFormat.new();
-		pong_format.width = render_size.x;
-		pong_format.height = render_size.y;
-		pong_format.format = RenderingDevice.DATA_FORMAT_R16G16B16A16_SFLOAT;
-		pong_format.usage_bits = RenderingDevice.TEXTURE_USAGE_STORAGE_BIT | RenderingDevice.TEXTURE_USAGE_SAMPLING_BIT;
-		
-		pong_texture = rd.texture_create(pong_format, RDTextureView.new());
-		pong_size = render_size;
-	
 	# Thread groups
 	var x_groups : int = (render_size.x - 1) / 8 + 1;
 	var y_groups : int = (render_size.y - 1) / 8 + 1;
 	var z_groups : int = 1;
 	
 	# Push constant
-	var push_constant : PackedInt32Array = PackedInt32Array();
+	var push_constant : PackedFloat32Array = PackedFloat32Array();
 	push_constant.push_back(render_size.x);
 	push_constant.push_back(render_size.y);
-	push_constant.push_back(kernel_size);
-	push_constant.push_back(0);
+	push_constant.push_back(0.0);
+	push_constant.push_back(0.0);
 	
 	var color_buffer : RID = render_scene_buffers.get_color_layer(0);
+	var motion_vector_buffer : RID = render_scene_buffers.get_velocity_layer(0);
+	var depth_buffer : RID = render_scene_buffers.get_depth_layer(0);
 	
 	# Create a uniform set.
 	var color_buffer_uniform : RDUniform = RDUniform.new();
@@ -74,7 +62,19 @@ func _render_callback(effect_callback_type: int, render_data: RenderData) -> voi
 	color_buffer_uniform.binding = 0;
 	color_buffer_uniform.add_id(color_buffer);
 	
-	var uniform_set : RID = UniformSetCacheRD.get_cache(shader, 0, [ color_buffer_uniform ]);
+	# Depth cannot be bound as image2D due to being created without the proper usage flag, it must be accessed with a sampler2D instead
+	var depth_buffer_uniform : RDUniform = RDUniform.new();
+	depth_buffer_uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_SAMPLER_WITH_TEXTURE;
+	depth_buffer_uniform.binding = 1;
+	depth_buffer_uniform.add_id(nearest_sampler);
+	depth_buffer_uniform.add_id(depth_buffer);
+	
+	var motion_buffer_uniform : RDUniform = RDUniform.new();
+	motion_buffer_uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_IMAGE;
+	motion_buffer_uniform.binding = 2;
+	motion_buffer_uniform.add_id(motion_vector_buffer);
+	
+	var uniform_set : RID = UniformSetCacheRD.get_cache(shader, 0, [ color_buffer_uniform, depth_buffer_uniform, motion_buffer_uniform ]);
 
 	# Run our compute shader.
 	var compute_list := rd.compute_list_begin();
@@ -123,9 +123,6 @@ func _notification(what):
 		
 		if nearest_sampler.is_valid():
 			rd.free_rid(nearest_sampler);
-		
-		if pong_texture.is_valid():
-			rd.free_rid(pong_texture);
 
 
 const template_shader: String = """
@@ -135,12 +132,13 @@ const template_shader: String = """
 layout(local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
 
 layout(rgba16f, set = 0, binding = 0) uniform image2D color_image;
+layout(set = 0, binding = 1) uniform sampler2D depth_image;
+layout(rgba16f, set = 0, binding = 2) uniform image2D motion_image;
 
 // Our push constant
 layout(push_constant, std430) uniform Params {
-	ivec2 raster_size;
-	int kernel_size;
-	int reserved;
+	vec2 raster_size;
+	vec2 reserved;
 } params;
 
 // The code we want to execute in each invocation
@@ -159,28 +157,11 @@ void main() {
 	
 	float raw_depth = texture(depth_image, uv).r;
 
-	int kernel_size = params.kernel_size;
 	
-	int samples = 1;
-	vec4 color_sum = color;
-	for (int x = -kernel_size; x <= kernel_size; ++x) {
-		for (int y = -kernel_size; y <= kernel_size; ++y) {
-			if (x == 0 && y == 0) continue;
-			
-			ivec2 sample_pos = thread_id + ivec2(x, y);
-			
-			sample_pos = clamp(sample_pos, ivec2(0), size);
-			
-			color_sum += imageLoad(color_image, sample_pos);
-			samples += 1;
-		}
-	}
-	
-	
-	vec4 color_output = color_sum / vec4(samples);
+	vec4 color_output = color;
 	vec4 depth_output = vec4(raw_depth);
 	vec4 motion_output = abs(motion);
 
-	imageStore(color_image, thread_id, color_output);
+	imageStore(color_image, thread_id, motion_output);
 }
 """
